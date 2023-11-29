4376e0fdfb3835de541f7c54275bc4c2d0c259a7
Ezequiel Carbonell <ezequielcarbonell3@gmail.com>
2023-11-28 22:04:46 -0300

    primera parte

---
+++ assets/javasript/JScript.js
@@ -0,0 +1,16 @@
+// document.getElementById("boton").addEventListener("click", function () {
+//     document.getElementById("Mtexto").style.display="block";
+// })
+
+// var faker = required('faker'); // Faker.js
+
+// api.customer  = {
+//     id:    random.special(4, 8),
+//     name:  faker.name.findName(),
+//     phone: faker.phone.phoneNumber("(###) ###-####"),
+//     address: {
+//         street: faker.address.streetAddress(),
+//         city: faker.address.city(),
+//         state: faker.address.state(),
+//     }
+// };
